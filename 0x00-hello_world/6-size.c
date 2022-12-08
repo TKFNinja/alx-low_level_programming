70ed11e57bbaa09aed6d48ca38e429af8bc5391d
TKFNinja <you@example.com>
2022-12-08 10:54:42 -0800

    Ex6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,2 +1,14 @@
-#!/bin/bash
-puts(sizeof(char))
+#include <stdio.h>
+#include <libc6-dev-i386>
+/**
+ * main - Entry point of program
+ * Return: 0
+*/
+int main(void)
+{
+	printf("Size of a char: %ld byte(s)\n", sizeof(char));
+	printf("Size of an int: %ld byte(s)\n", sizeof(int));
+	printf("Size of a long int: %ld byte(s)\n", sizeof(long int));                 printf("Size of a long long int: %ld byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %ld bytes\n", sizeof(float));
+        return 0;
+}
