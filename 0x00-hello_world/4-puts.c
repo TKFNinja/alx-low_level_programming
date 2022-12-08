365775eb1d6cbe14a7dd14ed057334f02d0e9db5
TKFNinja <you@example.com>
2022-12-08 10:18:07 -0800

    Ex4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,12 @@
-#!/bin/bash;
-puts("\"Programming is like building a multilingual puzzle");
+#include <stdio.h>
+
+/**
+ * main program
+ * returns 0
+*/
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
