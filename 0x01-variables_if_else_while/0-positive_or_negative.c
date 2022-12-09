daa5fe35810ccbe58751b9e532f18d8521c27f61
TKFNinja <you@example.com>
2022-12-09 15:37:40 -0800

    Ex0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -11,17 +11,17 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 
 /* your code goes there */
-	if (n>0);
+	if (n>0)
 	{
 		printf("%ld is positive\n", n);
 
-		else;
+		else
 		{
-			if (n=0);
+			if (n=0)
 			{
 				printf("%ld is zero\n", n);
 
-				else;
+				else
 					printf("%ld is negative\n", n);
 			}
 		}
