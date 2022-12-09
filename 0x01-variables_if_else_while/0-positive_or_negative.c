06efe40d3cee872f07a44599fd946cb4baf042c3
TKFNinja <you@example.com>
2022-12-09 15:51:12 -0800

    Ex0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -5,14 +5,14 @@
 /**
  * main - Entry point
  * Return: 0
-*/ 
+*/
 /* betty style doc for function main goes there */
 int main(void)
 {
 	int n;
+
 	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-	
+	n = rand()-RAND_MAX/2;
 /* your code goes there */
 	if (n > 0)
 	{
