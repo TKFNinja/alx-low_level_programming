74c776ad3b8f1135f9f9d9a01ad65accca715956
TKFNinja <you@example.com>
2022-12-09 17:09:57 -0800

    Ex5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ * Return: 0
+*/
+int main(void)
+{
+	int n = 0;
+
+	while (n < 10)
+	{
+		puts(n);
+		n++;
+	}
+	return (0);
+}
