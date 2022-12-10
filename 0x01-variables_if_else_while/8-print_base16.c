fcdc968daeedb53c0c345bb616de89f5da2520f7
TKFNinja <you@example.com>
2022-12-09 18:09:50 -0800

    Ex8

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -9,15 +9,15 @@ int main(void)
 	int n = 0;
 	char hex = [a, b, c, d, e, f];
 
-	while (n < 10)
+	while (n < 16)
 	{
-		putchar((n % 10) + '0');
+		putchar((n % 16) + '0');
 		n++;
 	}
-	for (n = 10; n < 16; n++)
+	/*for (n = 10; n < 16; n++)
 	{
 		putchar(hex[(n % 10) + 1]);
-	}
+	}*/
 	putchar('\n');
 	return (0);
 }
