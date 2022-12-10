36577cf204e6925894c121e8665fa522fbe4df49
TKFNinja <you@example.com>
2022-12-09 18:28:35 -0800

    Ex9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ * Return: 0
+*/
+int main(void)
+{
+	int n = 0;
+
+	while (n < 9)
+	{
+		putchar(n + '0');
+		putchar(',');
+		putchar(' ');
+		n++;
+	}
+	putchar(n + '0');
+	putchar('\n');
+	return (0);
+}
