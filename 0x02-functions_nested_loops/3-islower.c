c4bc61cde8534cd25ba7aaf7bc78bdee29704f8b
TKFNinja <you@example.com>
2022-12-13 14:45:20 -0800

    Ex3

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+ * main - check the code.
+ * Return: 0
+*/
+
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
+
+int _islower(char c)
+{
+	char alphabet = 'a';
+
+	while (alphabet <= 'z')
+	{	
+		if (c == alphabet)
+		{
+			return (1);
+		}
+		else
+			alphabet++;
+	}
+	return (0);
+}
+
+int main(void)
+{
+	_islower('c');
+	return (0);
+}
