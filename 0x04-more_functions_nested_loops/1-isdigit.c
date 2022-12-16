5ed7f9b3acec1f6adfaae61928fed77da7d99620
TKFNinja <you@example.com>
2022-12-15 16:03:00 -0800

    Ex1

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -3,11 +3,10 @@
 * _isdigit - checks for a digit between 0 and 9 inclusive
 * Return: 1 on success and 0 otherwise
 */
-
 int _isdigit(int c)
 {
 	char dig = '0';
-	
+
 	while (dig <= '9')
 	{
 		if (c == dig)
@@ -19,4 +18,3 @@ int _isdigit(int c)
 	}
 	return (0);
 }
-
