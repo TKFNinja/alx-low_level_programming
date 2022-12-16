99603d97f07c330bba56b1f80592a4b458098f64
TKFNinja <you@example.com>
2022-12-15 16:29:26 -0800

    Ex4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_most_numbers - prints the numbers, from 0 to 9 except 2 and 4 followed by a new line
+ * Return: numbers
+*/
+
+void print_most_numbers(void)
+{
+	char num = '0';
+
+	while (num <= '9')
+	{
+		if (num != '2' && num != '4')
+		{
+			_putchar(num);
+		}
+		else
+			num++;
+	}
+	_putchar('\n');
+}
