c691306c47d752bba232d4887184abe05bed1373
TKFNinja <you@example.com>
2022-12-16 03:03:37 -0800

    Ex9

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -14,7 +14,7 @@ int main(void)
 
 	while (i < 100)
 	{
-		if (i % 3 == 0)
+		if ((i % 3 == 0) && (i % 5 != 0))
 		{
 			printf("Fizz ");
 		}
@@ -25,14 +25,12 @@ int main(void)
 				printf("Buzz ");
 			}
 			else
-			{
-				if ((i % 3 == 0) && (i % 5 == 0))
-				{
-					printf("FizzBuzz ");
-				}
-				else
-					printf("%d ", i);
-			}
+				printf("%d ", i);
+		}
+		
+		if ((i % 3 == 0) && (i % 5 == 0))
+		{
+			printf("FizzBuzz ");
 		}
 		i++;
 	}
