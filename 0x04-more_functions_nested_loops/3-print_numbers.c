b1c47b3fdb9a913c101d4a15d06141d98e4e9541
TKFNinja <you@example.com>
2022-12-15 16:22:39 -0800

    Ex3

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -6,11 +6,11 @@
 
 void print_numbers(void)
 {
-	int num = 0;
+	char num = '0';
 
-	while (num < 10)
+	while (num <= '9')
 	{
-		_putchar(num);
+		_putchar(num + '0');
 		num++;
 	}
 	_putchar('\n');
