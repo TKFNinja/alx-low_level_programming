8a8e5e927f288f8ef5a2ceddc8e1fdfb74737306
TKFNinja <you@example.com>
2022-12-15 16:18:02 -0800

    Ex3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+* print_numbers - prints the numbers, from 0 to 9, followed by a new line
+* Return: integers
+*/
+
+void print_numbers(void)
+{
+	int num = 0;
+
+	while (num < 10)
+	{
+		_putchar(num);
+		num++;
+	}
+	_putchar('\n');
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -13,3 +13,4 @@ void print_alphabet_x10(void);
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
+void print_numbers(void);
