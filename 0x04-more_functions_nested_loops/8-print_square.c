129a93f4042ce925e0d86ea4dfcb695bc4b4be31
TKFNinja <you@example.com>
2022-12-16 01:25:40 -0800

    Ex8

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_square - prints a square, followed by a new line
+ * @size: is the size of the square
+ *
+ * Return: the square and a new line if the size is 0 or less
+*/
+
+void print_square(int size)
+{
+	int i = 0;
+
+	while (size--)
+	{
+		while (i < size)
+		{
+			_putchar('#');
+			i++
+		}
+		_putchar('\n')
+	}
+}	
