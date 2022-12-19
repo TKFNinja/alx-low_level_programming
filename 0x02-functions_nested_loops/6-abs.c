91e2649dd8f15cff3d21fde2a4b945e5dc3724d1
TKFNinja <tkforsab@gmail.com>
2022-12-19 11:20:41 +0000

    Ex6

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -3,7 +3,7 @@
  * _abs - computes the absolute value of an integer.
  * @int the interger whose absolute value is sort for
  *Return: the absolute value
-*/ 
+*/
 
 int _abs(int n)
 {
@@ -13,4 +13,4 @@ int _abs(int n)
 	}
 	else
 		return (-n);
-}	
+}
