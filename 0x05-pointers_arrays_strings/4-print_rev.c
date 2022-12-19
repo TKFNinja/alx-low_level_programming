09d49738a6d63e3f632dbe08de0810c61bfcd6d8
TKFNinja <tkforsab@gmail.com>
2022-12-19 21:55:40 +0000

    Ex4

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -8,8 +8,14 @@
 void print_rev(char *s)
 {
 	int i = 0;
+	int length = 0;
 
-	for (i = _strlen(s) - 1 ; i >= 0 ; i--)
+	while (s[length] != '\0')
+	{
+		length++;
+	}
+	
+	for (i = length - 1 ; i >= 0 ; i--)
 	{
 		_putchar(s[i]);
 	}
