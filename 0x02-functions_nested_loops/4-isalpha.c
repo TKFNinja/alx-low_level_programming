84827796fade24a671b451f5f9210ffae778cad0
TKFNinja <tkforsab@gmail.com>
2022-12-19 10:52:16 +0000

    Ex4

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -23,7 +23,10 @@ int _islower(int c)
 }
 
 /**
- * _isalpha - checks for alphabetic character. 
+ * _isalpha - checks for alphabetic character.
+ * @c: the letter to be checked
+ *
+ * Return: 1 if c is an alphabetic character and 0 otherwise 
 */ 
 
 int _isalpha(int c)
