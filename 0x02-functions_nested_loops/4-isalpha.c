3539b8f7186e63f1106426a20db60d301255796d
TKFNinja <tkforsab@gmail.com>
2022-12-19 10:44:24 +0000

    Ex4

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -6,9 +6,25 @@
  * Return: 1 if c is a letter and 0 otherwise
 */
 
-int _isalpha(int c)
+int _islower(int c)
 {
 	char alphabet = 'a';
+
+	while (alphabet <= 'z')
+	{
+		if (c == alphabet)
+		{
+			return (1);
+		}
+		else
+			alphabet++;
+	}
+	return (0);
+}
+
+int _isalpha(int c)
+{
+	char alphabet = 'A';
 
 	while (alphabet <= 'Z')
 	{
@@ -19,5 +35,5 @@ int _isalpha(int c)
 		else
 			alphabet++;
 	}
-	return (0);
+	_islower(c);
 }
