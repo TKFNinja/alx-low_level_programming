8853f8ce6b842b14956c3c85a54bf4cd75b16305
TKFNinja <tkforsab@gmail.com>
2022-12-19 10:14:55 +0000

    Ex2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -19,7 +19,8 @@ void print_alphabet(void)
 }
 
 /**
- * print_alphabet_x10 - prints 10 times the alphabet, in lowercase, followed by a new line
+ * print_alphabet_x10 - prints 10 times the alphabet, in lowercase,
+ * followed by a new line
 */
 
 void print_alphabet_x10(void)
