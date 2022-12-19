2bfdd99282e6c7ba82f6c6886e651bcbadaa5be9
TKFNinja <tkforsab@gmail.com>
2022-12-19 14:18:57 +0000

    Ex11

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,30 @@
+#include "stdlib.h"
+/**
+ * print_to_98 - prints all natural numbers from n to 98, followed by a new line
+ * @n: the number to start with
+ *
+ * Return: All the numbers
+*/
+
+void print_to_98(int n)
+{
+	int i;
+
+	if (n > 98)
+	{
+		for (i = n ; i >= 98 ; i--)
+		{
+			printf("%d, ", i);
+		}
+		printf("\n");
+	}
+	else
+	{
+		for (i = n ; i <= 98 ; i++)
+		{
+			printf("%d, ", i);
+		}
+		printf("\n");
+	}
+}
+
