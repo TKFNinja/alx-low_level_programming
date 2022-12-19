649fd242dd126151d8b58be5c3d271df4acc93ee
TKFNinja <tkforsab@gmail.com>
2022-12-19 22:08:47 +0000

    Ex5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * rev_string - reverses a string
+ * @s: the string to be reversed
+ * Return: void
+*/
+
+void rev_string(char *s)
+{
+	int i = 0;
+	int length = 0;
+	char c;
+
+	while (s[length] != '\0')
+	{
+		length++;
+	}
+
+	for (i = length - 1 ; i >= 0 ; i--)
+	{
+		c = s[length - 1 - i];
+		s[length - 1 - i] = s[i];
+		s[i] = c;
+	}
+}
