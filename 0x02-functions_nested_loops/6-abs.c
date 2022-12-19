d5a0667fbe54a82ca4dc59df6caf8601f651c941
TKFNinja <tkforsab@gmail.com>
2022-12-19 11:19:47 +0000

    Ex6

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -5,12 +5,12 @@
  *Return: the absolute value
 */ 
 
-int _abs(int)
+int _abs(int n)
 {
-	if (int >= 0)
+	if (n >= 0)
 	{
-		return (int);
+		return (n);
 	}
 	else
-		return (-int);
+		return (-n);
 }	
