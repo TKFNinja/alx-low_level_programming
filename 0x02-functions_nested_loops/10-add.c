98546c4146e3ed93b46fa4fb37ac1e177846a9b5
TKFNinja <tkforsab@gmail.com>
2022-12-19 14:01:06 +0000

    Ex10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * add -  adds two integers and returns the result.
+ * @n, m: the intergers to be added
+ *
+ * Return: the result
+*/
+
+int add (int n, int m)
+{
+	return (n + m);
+}	
