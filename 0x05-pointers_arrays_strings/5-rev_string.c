bc42cb16577dc29b8638e26b751782c4810c4ecd
TKFNinja <tkforsab@gmail.com>
2022-12-19 22:21:26 +0000

    Ex5

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -10,13 +10,13 @@ void rev_string(char *s)
 	int i = 0;
 	int length = 0;
 	char c;
-
+	
 	while (s[length] != '\0')
 	{
 		length++;
 	}
 
-	for (i = length - 1 ; i >= 0 ; i--)
+	for (i = 0 ; i <= length - 1 ; i++)
 	{
 		c = s[length - 1 - i];
 		s[length - 1 - i] = s[i];
