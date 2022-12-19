915e7e84d0e8f831cdc77135ba96d1904f91545f
TKFNinja <tkforsab@gmail.com>
2022-12-19 11:15:22 +0000

    Ex6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _abs - computes the absolute value of an integer.
+ * @int the interger whose absolute value is sort for
+ *Return: the absolute value
+*/ 
+
+int _abs(int)
+{
+	if (int >= 0)
+	{
+		return (int);
+	}
+	else
+		return (-int);
+}	
