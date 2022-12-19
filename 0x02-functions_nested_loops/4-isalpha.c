af020f664735debbc5d52255a50180d45690ca6c
TKFNinja <tkforsab@gmail.com>
2022-12-19 10:35:53 +0000

    Ex4

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,13 +1,10 @@
 #include "main.h"
 /**
- * main - check the code.
- * Return: 0
+ * _isalpha - checks for alphabetic character.
+ *@c: the letter to be checked
+ *
+ * Return: 1 if c is a letter and 0 otherwise
 */
-
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
-}
 
 int _isalpha(int c)
 {
@@ -22,19 +19,5 @@ int _isalpha(int c)
 		else
 			alphabet++;
 	}
-	return (0);
-}
-
-int main(void)
-{
-	int r;
-
-	r = _isalpha('H');
-	_putchar(r + '0');
-	r = _isalpha('o');
-	_putchar(r + '0');
-	r = _isalpha(108);
-	_putchar(r + '0');
-	_putchar('\n');
 	return (0);
 }
