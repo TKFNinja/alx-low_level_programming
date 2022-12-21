ab0f248253b3c87f9fec090717d9074e8f83b1e1
TKFNinja <tkforsab@gmail.com>
2022-12-21 00:25:49 +0000

    Ex10

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,54 @@
+#include "main.h"
+/**
+ * _atoi - converts a string to an integer.
+ * @s: string to be converted
+ *
+ * Return: integer
+*/
+
+int _atoi(char *s)
+{
+	int i = 0;
+	int j = 0;
+	int k = 0;
+	int length = 0;
+	int negatives = 0;
+	int result = 0;
+
+	while (s[length] != '\0')
+	{
+		length++;
+	}
+
+	for (i = length - 1 ; i >= 0 ; i--)
+	{
+		while (j <= 9)
+		{
+			if (s[i] == j)
+			{
+				result = result + (j * (10 ^ k));
+				k++;
+			}
+		
+			j++;
+		}
+	}
+
+	j = 0;
+	i = 0;
+	k--;
+
+	while (s[i] != (result / (10 ^ k)))
+	{
+		if (s[i] == '-')
+		{
+			negatives++;
+		}
+	}
+	if ((negatives % 2) == 0)
+	{
+		return (result);
+	}
+	else
+		return (-result);
+}		
