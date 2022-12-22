0a8247ea65849a2df13984d57edb6ef8d0bd6316
TKFNinja <tkforsab@gmail.com>
2022-12-22 18:42:01 +0000

    Ex1

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,33 @@
+#include "main.h"
+/**
+ * _strncat - concatenates two strings
+ * @src: the string to be used in appending
+ * @dest: the string to be appended
+ * @n: The number of bytes of src to be icluded in final string
+ * Return: a pointer to resulting string
+*/
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int i = 0;
+	int j = 0;
+
+	while (dest[j] != '\0')
+	{
+		j++;
+	}
+
+	while (src[i] != '\0')
+	{
+		if (i < n)
+		{
+			dest[j] = s[i];
+			i++;
+			j++;
+		}
+	}
+
+	dest[j] = '\0';
+
+	return (dest);
+}
