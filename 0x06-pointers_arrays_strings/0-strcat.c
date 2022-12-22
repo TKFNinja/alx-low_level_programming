90a06accf0b0d8b1dbbd52e1863a7f186be7595f
TKFNinja <tkforsab@gmail.com>
2022-12-22 17:44:48 +0000

    Ex0

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * _strcat - concatenates two strings
+ * @src: the string to be used in appending
+ * @dest: the string to be appended
+ * Return: a pointer to resulting string
+*/
+
+char *_strcat(char *dest, char *src)
+{
+	int i = 0;
+	int length = 0;
+
+	while (dest[length] != '\0')
+	{
+		length++;
+	}
+
+	while (src[i] != '\0')
+	{
+		dest[length] = src[i];
+		lenghth++;
+		i++;
+	}
+
+	result[j] = '\0';
+
+	return (dest);
+}
