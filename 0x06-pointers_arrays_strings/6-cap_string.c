58ed9b80cf836ae46e652e1da94e3a8ca973865f
TKFNinja <tkforsab@gmail.com>
2022-12-23 23:36:15 +0000

    Ex6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -11,7 +11,8 @@ char *cap_string(char *str)
 	int i = 0;
 	int j = 0;
 	char test = 'a';
-	char words[] = {' ', '\t', '\n', ',', ';', '.', '!', '?', '"', '(', ')', '{', '}'};
+	char words[] = {' ', '\t', '\n', ',', ';', '.', '!', '?',
+	       		'"', '(', ')', '{', '}'};
 
 	while (str[j] != '\0')
 	{
