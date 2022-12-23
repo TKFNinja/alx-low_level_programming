845c9cd87aa502765c7d1d37f2380a25fe50f86a
TKFNinja <tkforsab@gmail.com>
2022-12-23 22:52:13 +0000

    Ex6

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,43 @@
+#include "main.h"
+/**
+ * cap_string - capitalizes all words of a string.
+ * @str: the sample string
+ *
+ * Return: the capitalised string
+*/
+
+char *cap_string(char *str)
+{
+	int i = 0;
+	int j = 0;
+	char test = 'a';
+	char words[] = {' ', '\t', '\n', ',', ';', '.', '!', '?', '"', '(', ')', '{', '}'};
+
+	while (str[j] != '\0')
+	{
+		for (i = 0 ; i < 13 ; i++)
+		{
+			if (str[j] == words[i])
+			{
+				j++;
+				for (test = 'a' ; test <= 'z' ; test++)
+				{
+					if (str[j] == test)
+					{
+						str[j] = str[j] - 32;
+					}
+				}
+			}
+		}
+		j++;
+	}
+
+	for (test = 'a' ; test <= 'z' ; test++)
+	{
+		if (str[o] == test)			
+		{
+			str[o] = str[o] - 32;
+		}
+	}
+	return (str);
+}
