2bd394b40c72f4489a5bbeed57621a9b26d44e69
TKFNinja <tkforsab@gmail.com>
2022-12-23 22:03:03 +0000

    Ex5

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * string_toupper - changes all lowercase letters of a string to uppercase.
+ * @str: the sample string
+ * 
+ * Return: the string in uppercase
+*/
+
+char *string_toupper(char *str)
+{
+	int i = 0;
+	char test = 'a';
+	
+	while (str[i] != '\0')
+	{
+		if (str[i] == test)
+		{
+			str[i] = str[i] - 32;
+		}
+		test++;
+		i++
+	}
+	return (str);
+}
