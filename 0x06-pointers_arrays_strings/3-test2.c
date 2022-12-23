367b785b07cf8b2cfa1d0a2914a02bea903a1d12
TKFNinja <tkforsab@gmail.com>
2022-12-23 20:12:45 +0000

    Ex3

--- 0x06-pointers_arrays_strings/3-test2.c
+++ 0x06-pointers_arrays_strings/3-test2.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 /**
  * _strcmp - compares two strings
  * @s1: the sample string
@@ -42,7 +43,7 @@ int _strcmp(char *s1, char *s2)
 				}
 				else
 				{
-					if ((s1[k] / 10 + '0') < (s2[k] / 10 + '0'))
+					if ((s1[k] / 10 + '0') < (s2[k] / 10 + '0')) 
 					{
 						return (-15);
 					}
@@ -52,4 +53,15 @@ int _strcmp(char *s1, char *s2)
 			}
 		}
 	}
+}
+
+int main(void)
+{
+    char s1[] = "Holberton";
+    char s2[] = "Holbeerton";
+
+    printf("%d\n", _strcmp(s1, s2));
+    printf("%d\n", _strcmp(s2, s1));
+    printf("%d\n", _strcmp(s1, s1));
+    return (0);
 }
