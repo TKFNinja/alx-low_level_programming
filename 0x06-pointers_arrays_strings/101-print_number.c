caccbebb285eb69bd63d363dc8abdaf6ee178010
TKFNinja <tkforsab@gmail.com>
2022-12-24 01:38:37 +0000

    Ex9

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -9,14 +9,14 @@ void print_number(int n)
 {
 	if (n < 10 && n > -10)
 	{
-		_putchar(n % 10 + '0');
+		_putchar((n % 10) + '0');
 	}
 	else
 	{
 		if (n >= 10 || n <= -10)
 		{
-			_putchar(n / 10 + '0');
-			_putchar(n % 10 + '0');
+			_putchar((n / 10) + '0');
+			_putchar((n % 10) + '0');
 		}
 	}
 }
