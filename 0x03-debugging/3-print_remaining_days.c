6bea1e93422f680a5fded83e40a26564a1e7d40c
TKFNinja <tkforsab@gmail.com>
2022-12-25 22:47:00 +0000

    Ex3

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -29,13 +29,13 @@ void print_remaining_days(int month, int day, int year)
 			{
 				printf("Day of the year: %d\n", day);
 				printf("Remaining days: %d\n", 366 - day);
+				day++;
 			}
 			else
 				printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
 		}
 		else if (month == 12 && (year % 4 == 0))
 		{
-			day++;
 			printf("Day of the year: %d\n", day);
 			printf("Remaining days: %d\n", 366 - day);
 		}
