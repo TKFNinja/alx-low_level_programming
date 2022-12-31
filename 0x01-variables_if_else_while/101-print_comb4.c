34a4493fc36b1b1b34f70374b8611d208c6d92a3
TKFNinja <tkforsab@gmail.com>
2022-12-31 10:10:50 +0000

    Ex11

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -6,15 +6,15 @@
 */
 int main(void)
 {
-	int n = 0;
+	int n = 11;
 
 	while (n < 1000)
 	{
-		if (n / 100 < ((n % 100) / 10)) && (((n % 100) / 10) < n % 10)
+		if ((n / 100 < ((n % 100) / 10)) && (((n % 100) / 10) < n % 10))
 		{
 			putchar(n / 100 + '0');
 			putchar(((n % 100) / 10) + '0');
-			putchar(n % 10);
+			putchar(n % 10 + '0');
 			if (n < 789)
 			{
 				putchar(',');
