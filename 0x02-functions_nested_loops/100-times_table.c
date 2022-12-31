a8585647a3287f29901d41fb04946c8ce862bf70
TKFNinja <tkforsab@gmail.com>
2022-12-31 10:44:54 +0000

    Ex12

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,38 @@
+#include "main.h"
+/**
+ * times_table - prints the n time table
+ * Returns: ntimes table
+*/
+
+void print_times_table(int n)
+{
+	int i, j;
+	
+	if (n <= 15 && n >= 0)
+	{
+		for (i = 0 ; i <= n ; i++)
+		{
+			for (j = 0 ; j <= n ; j++)
+			{
+				if ((i * j) < 10)
+				{
+					_putchar((i * j) + '0');
+				}
+				else
+				{
+					_putchar(((i * j) / 10) + '0');
+					_putchar(((i * j) % 10) + '0');
+				}
+
+				if (j < n)
+				{
+					_putchar(',');
+					_putchar(' ');
+					if ((i * (j + 1)) < 10)
+						_putchar(' ');
+				}
+			}
+			_putchar('\n');
+		}
+	}
+}
