da86c74e318424c7da486eecdb1229841e6d4632
TKFNinja <tkforsab@gmail.com>
2023-01-01 21:06:52 +0000

    Ex16

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - prints the first 98 Fibonacci numbers
+ * Return: 0
+*/
+
+int main(void)
+{
+	long int Fibo1 = 1;
+	long int n = 0;
+	long int Fibo2 = 1;
+	int m = 0;
+
+	while (m < 98)
+	{
+		n = Fibo2;
+		Fibo2 = Fibo2 + Fibo1;
+		Fibo1 = n;
+		if (m < 97)
+			printf("%ld, ", Fibo1);
+		if (m == 97)
+			printf("%ld", Fibo1);
+		m++;
+	}
+	printf("\n");
+	return (0);
+}
