81746989296531fa8822f03e1fef5f98aa259327
TKFNinja <tkforsab@gmail.com>
2023-01-01 20:26:31 +0000

    Ex13

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,10 +1,9 @@
-#include "main.h"
 #include <stdlib.h>
 #include <stdio.h>
 /**
  * main - computes and prints the sum of all the multiples of 
  * 3 or 5 below 1024
- * Returns: the sum of all the multiples of 3 or 5 below 1024
+ * Returns: 0
 */
 
 int main(void)
@@ -20,6 +19,6 @@ int main(void)
 		}
 		n++;
 	}
-	printf("%d", sum);
+	printf("%d\n", sum);
 	return (0);
 }
