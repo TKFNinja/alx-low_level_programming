31ef4ed62076dc525b77f0b72ac7430f9475fc4e
TKFNinja <tkforsab@gmail.com>
2023-01-02 05:02:12 +0000

    Ex0

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * _memset - fills the first n bytes of the memory area pointed
+ * to by s with the constant byte b
+ * @n: number of bytes to fill
+ * @s: pointer to a memory portion
+ * @b: char to fill in the memory area
+ * Return: a pointer to s
+*/
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	int i = 0;
+	char *t;
+
+	while (i < n)
+	{
+		s[i] = b;
+		i++;
+	}
+	t = &s;
+	return (t);
+}
