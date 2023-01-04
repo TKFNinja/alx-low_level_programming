93a04fc9d9a8c57f3e4bf4f91242544f9984a45d
TKFNinja <tkforsab@gmail.com>
2023-01-04 00:25:46 +0000

    Ex5

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,39 @@
+#include "main.h"
+/**
+ * _strstr - locates a substring.
+ * @haystack: string to be searched
+ * @needle: bytes to be located in haystack
+ * Return: a pointer to the beginning of haystack 
+*/
+
+char *_strstr(char *haystack, char *needle)
+{
+	unsigned long int i, j, k = 0;
+	char *locate;
+
+	while (haystack[i] != '\0')
+	{
+		if (haystack[i] == needle[0])
+		{
+			j = 0;
+			k = i;
+			while (needle[j] != '\0')
+			{
+				if (haystack[k] == needle[j])
+				{
+					j++;
+					k++;
+				}
+				else
+					break;
+			}
+			if (needle[j] == '\0')
+			{
+				locate = &haystack[i];
+				return (locate);
+			}
+		}
+		i++;
+	}
+	return (NULL);
+}
