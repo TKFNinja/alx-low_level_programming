3caff1f56e068119de960d43189d9efeade029ac
TKFNinja <tkforsab@gmail.com>
2023-01-04 01:28:27 +0000

    Ex6

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -8,14 +8,14 @@
 
 void print_chessboard(char (*a)[8])
 {
-	int i, j, k = 0;
+	int i, j = 0;
 
 	while (i < 8)
 	{
 		j = 0;
 		while (j < 8)
 		{
-			_putchar(a[j]);
+			_putchar(a[i][j]);
 			j++;
 		}
 		_putchar('\n');
